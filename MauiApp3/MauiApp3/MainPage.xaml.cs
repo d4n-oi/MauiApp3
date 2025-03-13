e71f149a77b8e81d06c48dcad03b47ad38708a20
Dant38 <Fudansh**1>
2025-03-13 16:35:23 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp3/MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            /*count++;]
+             * int count = 0;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);*/
+        }
+
+        private void btnProduto_Clicked(object sender, EventArgs e)
+        {
+            Produto produto = new Produto();
+
+            produto.Nome = "Batatinha";
+            produto.Preco = 2.90;
+            produto.Categoria = "Salgadinho";
+
+            Navigation.PushAsync(new ProdutoPage() { BindingContext = produto });
+        }
+
+
+        private void btnListaProduto_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListaProdutoPage());
+        }
+    }
+
+}
---
+++ MauiApp3/MauiApp3/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3
+{
+    class Produto
+    {
+        public double Preco { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+
+        public List<Produto> Lista 
+        {
+            get
+            {
+                List<Produto> lista = new List<Produto>();
+                lista.Add(new Produto() { Nome = "Cheetos", Preco = 6.50, Categoria = "Salgadinho"});
+                lista.Add(new Produto() { Nome = "Maria-Mole", Preco = 7, Categoria = "Docinho"});
+                lista.Add(new Produto() { Nome = "Caixa de goodgood", Preco = 19.99, Categoria = "Docinho"});
+                lista.Add(new Produto() { Nome = "Barra de chocolate", Preco = 15, Categoria = "Docinho" });
+                lista.Add(new Produto() { Nome = "Baconzitos", Preco = 5, Categoria = "Salgadinho"});
+                lista.Add(new Produto() { Nome = "Paçoca", Preco = 0.80, Categoria = "Docinho" });
+                lista.Add(new Produto() { Nome = "Teste", Preco = 0.50, Categoria = "Salgadinho"});
+                lista.Add(new Produto() { Nome = "aaaaa", Preco = 20, Categoria = "Salgadinho"});
+
+                return lista;
+            } 
+        }
+    }
+}
